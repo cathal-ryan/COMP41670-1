1ec4095f213ef397a63a4fe5e21f47777e663618
Conor <conor.kneafsey@ucdconnect.ie>
2020-12-12 12:40:17 +0000

    added get and set player locations

--- Forbidden Island Cards/src/pawns/Pawn.java
+++ Forbidden Island Cards/src/pawns/Pawn.java
@@ -1,14 +1,26 @@
 package pawns;
 
+import java.awt.Point;
+
 import enums.TilesEnums;
 
 abstract public class Pawn {
+
+    protected Point location;
 
     protected Pawn() {
 	}
     abstract public String getPlayerType();
 
     abstract public TilesEnums startLoc();
+
+    public void setLoc(Point p) {
+        this.location = new Point(p);
+    }
+
+    public Point getLoc() {
+        return location;
+    }
     
     public void shoreUp() {
         System.out.println("Some day I'll get around to shoring up");
