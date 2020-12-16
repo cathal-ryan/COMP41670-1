b5abf3f7716895c4a953d4e1530260b02f5edac3
Conor <conor.kneafsey@ucdconnect.ie>
2020-12-16 00:19:18 +0000

    can now draw tiles

--- Forbidden Island Cards/src/board/DrawTile.java
+++ Forbidden Island Cards/src/board/DrawTile.java
@@ -12,13 +12,18 @@ import enums.TilesEnums;
 
 public class DrawTile {
 
+	// number of rows in Tile Drawing
 	final int row = 6;
-	final int col = 20;
+	// number of columns of tiles in Board
+	final int col = 6;
 
 	List<List<String>> lst2d;
 
-	public DrawTile(Tile t) {
+	public DrawTile() {
 		this.lst2d = new ArrayList<List<String>>();
+	}
+
+	public void createTile(Tile t) {
 		createPerim(t);
 		writeName(t);
 		writeType(t);
@@ -29,20 +34,20 @@ public class DrawTile {
 			List<String> lst = new ArrayList<String>();
 			if(t.getType() == TypeEnums.SEA) {
 				if(i == 0 || i==row-1)
-					lst.add("wwwwwwwwwwwwwwwwwwww");
+					lst.add("wwwwwwwwwwwwwwwwwwww ");
 				else {
 					lst.add("w");
 					lst.add("                  ");
-					lst.add("w");
+					lst.add("w ");
 				}
 			}
 			else {
 				if(i == 0 || i==row-1)
-					lst.add("xxxxxxxxxxxxxxxxxxxx");
+					lst.add("xxxxxxxxxxxxxxxxxxxx ");
 				else {
 					lst.add("x");
 					lst.add("                  ");
-					lst.add("x");
+					lst.add("x ");
 				}
 			}
 			lst2d.add(i,lst);
@@ -57,15 +62,30 @@ public class DrawTile {
 		lst2d.get(2).set(1,t.getType().toString());
 	}
 
-	private List<List<String>> get2dList() {
+	public List<List<String>> get2dList() {
 		return lst2d;
+	}
+
+	private void dispRow(int tileNum, int line) {
+		List<String> list = lst2d.get(tileNum*row+line);
+		for(String s : list)
+			System.out.print(s);
+	}
+
+	public void dispAllRows() {
+		for(int line=0; line<row; line++) {
+			for(int tileNum=0; tileNum<col; tileNum++)
+				dispRow(tileNum, line);
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) {
 		Tile newTile = new Tile(0,0);
 		newTile.setName(TilesEnums.SEA);
 		newTile.setType(TilesEnums.SEA);
-		DrawTile dTile = new DrawTile(newTile);
+		DrawTile dTile = new DrawTile();
+		dTile.createTile(newTile);
 		List<List<String>> testList = dTile.get2dList();
 		for(List<String> list : testList) {
 			for(String s : list) {
@@ -77,8 +97,8 @@ public class DrawTile {
 		Tile tile2 = new Tile(1,0);
 		tile2.setName(TilesEnums.CAVE_OF_SHADOWS);
 		tile2.setType(TilesEnums.CAVE_OF_SHADOWS);
-		DrawTile dTile2 = new DrawTile(tile2);
-		List<List<String>> testList2 = dTile2.get2dList();
+		dTile.createTile(tile2);
+		List<List<String>> testList2 = dTile.get2dList();
 		System.out.println("\nPrinting tile2");
 		for(List<String> list : testList2) {
 			for(String s : list) {
@@ -86,6 +106,9 @@ public class DrawTile {
 			}
 			System.out.println();
 		}
+
+		System.out.println("New disp test");
+		dTile.dispAllRows();
 		System.out.println(testList2.get(1).get(1));
 	}
 }
