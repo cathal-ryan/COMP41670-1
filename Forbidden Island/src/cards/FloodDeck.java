eb6ad3f5fdfde0a2eec92a149551b8725ec6bd44
cathal-ryan <rmcathal@gmail.com>
2020-12-22 19:54:04 +0000

    Slight structural changes to TreasureDeck class

--- Forbidden Island/src/cards/FloodDeck.java
+++ Forbidden Island/src/cards/FloodDeck.java
@@ -13,24 +13,33 @@ import enums.TileStack;
 public class FloodDeck extends Deck{
 	protected static FloodDeck theFloodDeck;
 
-	
+    /**
+     * getInstance singleton method gets single instance
+     * of the Flood Deck.
+     * @return Singleton FloodDeck object
+     */
 	public static FloodDeck getInstance(){
         if(theFloodDeck == null){
             theFloodDeck = new FloodDeck();
         }
         return theFloodDeck;
-    }
+	}
 	
+	/** Constructor private for singleton
+     */
 	private FloodDeck() {
-		TileStack names = new TileStack();
+		TileStack names = new TileStack(); // Tile stack will handle getting names of all cards, same as tiles 
 
 		// Prepare empty array of Cards
 	    this.cardsInDeck = new Stack<Card>();
-		while(names.size() >= 1) {
+		while(names.size() >= 1) { // add all tiles to the stack of cards
 			cardsInDeck.push(new FloodCard(names.pop()));
 		}
 	}
 
+	/** Returns a floodCard from the stack
+	 * @return FloodCard object from the floodDeck
+     */
 	public Card dealCard() {
 		if(cardsInDeck.size()<1) {
 			FloodDiscardPile.getInstance().putbackall();
--- Forbidden Island/src/cards/TreasureDeck.java
+++ Forbidden Island/src/cards/TreasureDeck.java
@@ -2,62 +2,82 @@ package cards;
 import java.util.Stack;
 import enums.TreasureCardEnums;
 
+/**
+ * Treasure Deck class for decks of treasure cards, inherits from Deck
+ * Implemented as singleton as there should be only a single treasure deck
+ * 
+ * @author Cathal Ryan and Conor Kneafsey
+ *
+ */
 public class TreasureDeck extends Deck{
-		protected static TreasureDeck theTreasureDeck;
+	protected static TreasureDeck theTreasureDeck; // singleton object
 
-		public static TreasureDeck getInstance(){
-	        if(theTreasureDeck == null){
-	            theTreasureDeck = new TreasureDeck();
-	        }
-	        return theTreasureDeck;
-	    }
+	/**
+     * getInstance singleton method gets single instance
+     * of the Treasure Deck.
+     * @return Singleton FloodDeck object
+     */
+	public static TreasureDeck getInstance(){
+		if(theTreasureDeck == null){
+			theTreasureDeck = new TreasureDeck();
+		}
+		return theTreasureDeck;
+	}
+	
+	/**
+	 * Private constructor creates deck stack
+	 */
+	private TreasureDeck() {
+		// Prepare empty array of Cards
+		this.cardsInDeck = new Stack<Card>();
+		// Add in all the cards and shuffle
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
+
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
+
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
+
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
 		
-		private TreasureDeck() {
-		    // Prepare empty array of Cards
-		    this.cardsInDeck = new Stack<Card>();
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.EARTH_STONE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.WATERS_RISE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.WATERS_RISE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.WATERS_RISE));
 
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.CRYSTAL_OF_FIRE));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.SANDBAGS));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.SANDBAGS));
 
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.STATUE_OF_THE_WIND));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.HELICOPTER_LIFT));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.HELICOPTER_LIFT));
+		cardsInDeck.push(new TreasureCard(TreasureCardEnums.HELICOPTER_LIFT));		    
+		shuffle();
+	}
 
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.OCEANS_CHALICE));
-		    
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.WATERS_RISE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.WATERS_RISE));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.WATERS_RISE));
-
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.SANDBAGS));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.SANDBAGS));
-
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.HELICOPTER_LIFT));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.HELICOPTER_LIFT));
-		    cardsInDeck.push(new TreasureCard(TreasureCardEnums.HELICOPTER_LIFT));		    
-		    shuffle();
+	/** Returns a floodCard from the stack
+	* @return TreasureCard object from the treasure Deck
+	*/
+	public Card dealCard() {
+		if(cardsInDeck.size()<1) { // reset the deck if none left
+			TreasureDiscardPile.getInstance().putbackall();
+			return cardsInDeck.pop();
 		}
-		public Card dealCard() {
-			if(cardsInDeck.size()<1) {
-				TreasureDiscardPile.getInstance().putbackall();
-				return cardsInDeck.pop();
-			}
-			else {
-				return cardsInDeck.pop();
-			}
+		else {
+			return cardsInDeck.pop();
 		}
+	}
 }
